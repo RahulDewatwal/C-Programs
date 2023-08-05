d15fdf8b1623bc45c6ef8fa6fbce4d3be08412bc
RahulDewatwal <114424031+RahulDewatwal@users.noreply.github.com>
2023-08-05 10:36:03 +0530

    Add files via upload

---
+++ Space_in_memory_using_size_of_function_in_C.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+   printf("size of INT is %ld bytes",sizeof(int));
+   printf("\nsize of FLOAT is %ld bytes",sizeof(float));
+   printf("\nsize of CHAR is %ld bytes",sizeof(char));
+   printf("\nsize of DOUBLE is %ld bytes",sizeof(double));
+
+    return 0;
+    
+}
