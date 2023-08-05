8a1842e9d6b0388060bce298656cca13b3c29057
RahulDewatwal <114424031+RahulDewatwal@users.noreply.github.com>
2023-08-05 10:34:26 +0530

    Add files via upload

---
+++ Address_of_variable_in_c.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+   int a;//declaration
+   a=10;//assignment
+   printf("value of a is %p",&a);
+   a=20;
+   printf("\nvalues of a is now is %p",&a);
+    return 0;
+    
+}
