ad3e9be3269d8a0496054aceaf679d23dfb2037a
RahulDewatwal <114424031+RahulDewatwal@users.noreply.github.com>
2023-08-05 10:37:38 +0530

    Add files via upload

---
+++ Range_of_values_for_datatype_in_C.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<limits.h>
+#include<float.h>
+int main()
+{
+    printf("min range of int is %d", INT_MIN);
+    printf("\nmax range of int is %d", INT_MAX);
+    printf("\nmin range of short is %d", SHRT_MIN);
+    printf("\nmax range of short is %d", SHRT_MAX);
+    printf("min range of float is %f", FLT_MIN);
+    printf("\nmax range of float is %f", FLT_MAX);
+    printf("\nmin range of char is %d", CHAR_MIN);
+    printf("\nmax range of char is %d", CHAR_MAX);
+    printf("\nmin range of float is %e", FLT_MIN);
+    printf("\nmax range of float is %e", FLT_MAX);
+    return 0;
+}
