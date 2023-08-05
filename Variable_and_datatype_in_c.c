795efd22cf2ac4997d326be46e0ec960c4b7e841
RahulDewatwal <114424031+RahulDewatwal@users.noreply.github.com>
2023-08-05 10:33:20 +0530

    Add files via upload

---
+++ Variable_and_datatype_in_c.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+   int a;//declaration
+   a=10;//assignment
+   printf("value of a is %d",a);
+   a=20;
+   printf("\nvalues of a is now is %d",a);
+    return 0;
+    
+}
