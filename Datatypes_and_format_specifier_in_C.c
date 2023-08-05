fc5d07f44577ade1dd5eb3563e0550a28cfc90f0
RahulDewatwal <114424031+RahulDewatwal@users.noreply.github.com>
2023-08-05 11:10:37 +0530

    Add files via upload

---
+++ Datatypes_and_format_specifier_in_C.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a = 1;
+    float b = 5.2;
+    double c = 5.3;
+    char z = 'a';
+    printf ("value of a(int)=%d", a);
+    printf ("\nvalue of b(float)=%f", b);
+    printf ("\nvalue of c(double)=%.12f", c);
+    printf ("\nvalue of z(char)=%c",z);
+    
+    return 0;
+}
