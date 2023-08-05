2675608ed8502f9460e3a1933ad3b39d0ff52317
RahulDewatwal <114424031+RahulDewatwal@users.noreply.github.com>
2023-08-05 11:17:17 +0530

    Add files via upload

---
+++ Arithmatic_operator_in_C.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter 2 numbers=");
+    scanf("%d%d",&a,&b);
+    printf("sum is %d",a+b);
+    printf("\nDifference is %d",a-b);
+    printf("\nProduct is %d",a*b);
+    printf("\nDivision is %d",a/b);
+    printf("\nRemainder is %d",a%b);
+    return 0;
+}
