aa9a136f60827f07928d2c31a4ec5cf2d968b7f0
Rahul Dewatwal <114424031+RahulDewatwal@users.noreply.github.com>
2023-08-09 22:17:37 +0530

    Create Program_to_test_the_concept_of_pre-decrement.c

---
+++ Program_to_test_the_concept_of_pre-decrement.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+ int i=10;
+ printf("value of i before pre-decrement is %d",i);
+ printf("\nvalue of --i is %d",--i);
+ printf("\nvalue of i after pre-decrement is %d",i);
+ return 0;
+}
