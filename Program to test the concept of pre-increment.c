a4988367ca63b935094e1f8459f0c0e2f3cb077a
Rahul Dewatwal <114424031+RahulDewatwal@users.noreply.github.com>
2023-08-09 21:54:28 +0530

    Create Program_to_test_the_concept_of_pre-increment.c

---
+++ Program to test the concept of pre-increment.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+ int i=10;
+ printf("value of i before pre-increment is %d",i);
+ printf("\n value of i++ is %d",++i);
+ printf("\n value of i after pre-increment is %d",i);
+ return 0;
+}
