56787487e06756878d3eb743ef4a1cc06258fe8a
Rahul Dewatwal <114424031+RahulDewatwal@users.noreply.github.com>
2023-08-09 22:05:46 +0530

    Create Program_to_test_the_concept_of_post-decrement.c

---
+++ Program_to_test_the_concept_of_post-decrement.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+ int i=10;
+ printf("value of i before post-decrement is %d",i);
+ printf("\nvalue of i-- is %d",i--);
+ printf("\nvalue of i after post-decrement is %d",i);
+ return 0;
+}
