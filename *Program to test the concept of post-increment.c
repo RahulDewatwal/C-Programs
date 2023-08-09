69c6704befe9f439abb3db273365ade17c291ff9
Rahul Dewatwal <114424031+RahulDewatwal@users.noreply.github.com>
2023-08-09 21:53:30 +0530

    Create_Program_to_test_the_concept_of_post-increment.c

---
+++ *Program to test the concept of post-increment.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+ int i=10;
+ printf("value of i before post-increment is %d",i);
+ printf("\nvalue of i++ is %d",i++);
+ printf("\nvalue of i after post-increment is %d",i);
+return 0;
+}
